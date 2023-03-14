2cc7f5d866ac03233c9e3451c03eb6906ca87a42
MahmoudFadl243 <mahmoudfadl243@gmail.com>
2023-03-14 13:53:13 -0700

    message

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,40 +1,55 @@
+#include "holberton.h"
+#include <stdlib.h>
+
 /**
- * argstostr - concatenates all the arguments of your program
- * @ac: number of arguments
- * @av: array of arguments
+ * argstostr - concatenates all the arguments of a program
+ * @ac: number of arguments passed to the program
+ * @av: array of strings containing the arguments
  *
- * Return: pointer to the concatenated string
+ * Return: pointer to a new string, or NULL if it fails
  */
+
 char *argstostr(int ac, char **av)
 {
-    int i, j, len = 0, k = 0;
-    char *str;
+	int i, j, k, len;
+	char *str;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-            len++;
-        len++;
-    }
+	len = 0;
 
-    str = malloc(sizeof(char) * (len + 1));
-    if (str == NULL)
-        return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			len++;
+			j++;
+		}
+		len++;
+	}
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-        {
-            str[k] = av[i][j];
-            k++;
-        }
-        str[k] = '\n';
-        k++;
-    }
-    str[k] = '\0';
+	str = malloc(sizeof(char) * (len + 1));
 
-    return (str);
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			str[k] = av[i][j];
+			k++;
+			j++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
 }
